4df30b82fe27e82a2a612a2cccce2b8fa196260b
Pablitox07 <118412580+Pablitox07@users.noreply.github.com>
2023-11-23 13:03:03 -0600

    Update GTA.js

--- GTA.js
+++ GTA.js
@@ -1,20 +1,12 @@
 document.getElementById("addButton").addEventListener("click", addRow);
 
 function addRow() {
-  
-    
-    var tableBody = document.getElementById("tabla");
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td"); 
-    var tr = document.createElement("tr");
-    
-    td1.innerHTML = document.getElementById("nombreclave").value;
-    td2.innerHTML  = document.getElementById("codigoclave").value;
+    const path = "data.json";
+    fetch(path)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.error("error ", error));
+}
 
-    
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-
-    
-    tableBody.appendChild(tr);
-    }
